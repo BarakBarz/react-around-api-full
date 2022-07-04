26db8b635fc0280e9f3edbbae78d9c0726c28cf7
Barak Barzilay <barakoyb@gmail.com>
2022-07-04 22:12:50 +0300

    add feature\resonsive-header and fix title for .header__title_type_info

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,31 +1,65 @@
+import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import logo from '../images/Logo.svg';
+
 function Header({ isLoggedIn, userData, onClick }) {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const [headerHeightExtension, setHeaderHeightExtension] = useState(false);
+
+  const toggleNav = () => {
+    setToggleMenu(!toggleMenu);
+    setHeaderHeightExtension(!headerHeightExtension);
+  };
+
+  useEffect(() => {
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', changeWidth);
+
+    return () => {
+      window.removeEventListener('resize', changeWidth);
+    };
+  }, []);
+
   const linkRoute = useLocation().pathname === '/signin' ? '/signup' : 'signin';
   const linkText = linkRoute === '/signup' ? 'Sign up' : 'Log in';
+
   return (
-    <header className="header">
-      <img src={logo} alt="Around The U.S. Logo" className="logo" />
-      <div className="header__navbar">
-        {isLoggedIn ? (
-          <>
-            <p className="header__text">{userData}</p>
-            <Link
-              className={'header__link header__link_type_logout link'}
-              to={linkRoute}
-              onClick={onClick}
-            >
-              Log out
-            </Link>
-          </>
-        ) : (
-          <>
-            <Link className={'header__link link'} to={linkRoute}>
-              {linkText}
-            </Link>
-          </>
-        )}
+    <header className={headerHeightExtension ? 'header header_type_extended-height' : 'header'}>
+      <div className="header__content">
+        <img src={logo} alt="Around The U.S. Logo" className="logo" />
+        {isLoggedIn
+          ? (toggleMenu || screenWidth > 600) && (
+              <ul className="header__navbar">
+                <li className="header__text">{userData}</li>
+                <li>
+                  <Link
+                    className={'header__link header__link_type_logout link'}
+                    to={linkRoute}
+                    onClick={() => {
+                      onClick();
+                      toggleNav();
+                    }}
+                  >
+                    Log out
+                  </Link>
+                </li>
+              </ul>
+            )
+          : (toggleMenu || screenWidth > 600) && (
+              <ul className="header__navbar">
+                <li>
+                  <Link className={'header__link link'} to={linkRoute}>
+                    {linkText}
+                  </Link>
+                </li>
+              </ul>
+            )}
       </div>
+      {(toggleMenu || screenWidth) < 600 && <button onClick={toggleNav} className="header__nav-button"></button>}
     </header>
   );
 }
---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function NavBar() {
+  return (
+    <nav className="header__navbar">
+      <ul class="list">
+        <li class="item">Home</li>
+        <li class="item">Home</li>
+        <li class="item">Home</li>
+      </ul>
+    </nav>
+  );
+}
